4023566f42d5d9960297830ec0512de996d0e7a2
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-04 16:18:37 -0700

    Added questions.  Safe revert point.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,45 @@
 // TODO: Include packages needed for this application
+const inquirer = require(`inquirer`);
+const fs = require(`fs`);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type:`input`,
+    name:`projectTitle`,
+    message:`What is the title of your ?`
+},
+    {type:`input`,
+    name:`description`,
+    message:`Please write a brief description about the .`
+},
+    {type:`input`,
+    name:`installation`,
+    message:`Please write installation instructions.`
+},
+    {type:`input`,
+    name:`usage`,
+    message:`Please write instructions on how to use.`
+},
+    {type:`input`,
+    name:`contribution`,
+    message:`Please write instructions on how to contribute.`
+},
+    {type:`input`,
+    name:`tests`,
+    message:`Please write instructions on how to test the application.`
+},
+    {type:`checkbox`,
+    name:`license`,
+    message:[`MIT`, `None`]
+},
+    {type:`input`,
+    name:`userName`,
+    message:`What is your GitHub username?`
+},
+    {type:`input`,
+    name:`email`,
+    message:`What is your email?`
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
