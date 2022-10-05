46736568826bd3e29acef635895006ecb4d3cfc9
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-05 10:19:27 -0700

    Updated contents of the README template.  Safe revert point.

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-const { write } = require("fs");
+//const { write } = require("fs");
 const inquirer = require(`inquirer`);
 const { writeFile } = require(`fs`).promises;
 
@@ -42,25 +42,25 @@ const questions = [
     message:`What is your email?`
 }];
 
-const promptUser = ()=> {
-    return inquirer.prompt(questions)
-};
+const promptUser = ()=> inquirer.prompt(questions);
+
 
 const generateREADME = ({projectTitle, description, installation, usage, contribution, tests, license, userName, email}) => 
+
     `# ${projectTitle}
 
     ## Description
     
     ${description}
     
-    ## Table of Contents (Optional)
+    ## Table of Contents 
     
-    If your README is long, add a table of contents to make it easy for users to find what they need.
-    
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
+    - [Usage](#Usage)
+    - [Credits](#Credits)
+    - [License](#License)
+    - [How to Contribute](#How-to-Contribute)
+    - [Tests](#Tests)
+    - [Questions](#Questions)
     
     ## Installation
     
@@ -69,38 +69,28 @@ const generateREADME = ({projectTitle, description, installation, usage, contrib
     ## Usage
     
     ${usage}
-    <!---
+    
     ## Credits
     
-    List your collaborators, if any, with links to their GitHub profiles.
     
-    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
     
-    If you followed tutorials, include links to those here as well.
-    -->
     ## License
     
     ${license}
     
-    
-    <!---
-    ## Badges
-    
-    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-    
-    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-    
-    ## Features
-    
-    If your project has a lot of features, list them here.
-    -->
+
     ## How to Contribute
     
     ${contribution}
     
     ## Tests
     
-    ${tests}`
+    ${tests}
+    
+    ## Questions
+
+    If you have any further questions, concerns, or feedback feel free to email me at ${email}.  Another way you can contact me is through my [GitHub](${userName}) link. 
+    `
 ;
 
 // TODO: Create a function to write README file
